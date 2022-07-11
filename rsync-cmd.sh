30db49dfb86a7de50276bc624c66889c6c62911d
Alexander <java.git@yandex.ru>
2022-07-11 10:22:07 +0300

    all commits

--- rsync-cmd.sh
+++ rsync-cmd.sh
@@ -4,9 +4,9 @@
 if [[ ("$#" -eq 2) ]]; then
   rsync -avv --delete "$1/*" "$2" 2>&1 | tac | sed '1,4p;d' | grep -v -E '^s*(#|;|$)' | tac
   RC=$?
+  echo "(RC=${RC})"
 else
   echo -e 'Usage:\n rsync-cmd.sh "${CRONHOST}:/data/postgres2/10/data" "/cronwork" \n rsync-cmd.sh "/cronwork/*" "${CRONHOST}:/tmp/PG_BACKUP" '
   RC=-1
 fi
-
 exit ${RC}
