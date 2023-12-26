e088e47eb0417d44aa736e659a6734505ed252b3
Alexander <java.git@yandex.ru>
2023-12-26 22:11:53 +0300

    commit

--- rsync-cmd.sh
+++ rsync-cmd.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # rsync-cmd.sh
+LNPREFIX='[rsync] '
 
 if [[ ("$#" -eq 2) ]]; then
-  rsync -avv --delete $1/* $2 2>&1 | tail -n 4 | grep -v -E '^s*$' | ts '[rsync] '
+  rsync -avv --delete $1/* $2 2>&1 | tail -n 4 | grep -v -E '^s*$' | ts "${LNPREFIX}"
   RC=$?
-  echo "(RC=${RC})"
+  echo "(RC=${RC})" | ts "${LNPREFIX}"
 else
   echo -e 'Usage:\n rsync-cmd.sh ${CRONHOST}:/data/postgres2/10/data /cronwork \n rsync-cmd.sh /cronwork ${CRONHOST}:/tmp/PG_BACKUP '
   RC=-1
